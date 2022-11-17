5463b60ef976d720be51017da45d2393a8bc6258
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2022-11-17 23:49:23 +1100

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import '../styles/Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="about">
+                <h1> Hi, My Name is Ben </h1>
+                <div className="prompt">
+                    <p>A computer science graduate with a passion for learning and creating.</p>
+                    <LinkedInIcon onClick={() => window.open("https://www.linkedin.com/in/shyam-raj-652b55198/")} />
+                    <EmailIcon onClick={() => { window.open("mailto:shyamraj0805@gmail.com"); window.alert("Email: shyamraj0805@gmail.com"); }} />
+                    <GitHubIcon onClick={() => window.open("https://github.com/sraj2279")} />
+                </div>
+            </div>
+            <div className="skills">
+                <h1> Skills </h1>
+                <ol className="list">
+                    <li className="item1">
+                        <h2>Front-End</h2>
+                        <span>ReactJS, HTML, CSS, JavaScript</span>
+                    </li>
+                    <li className="item2">
+                        <h2>Back-End</h2>
+                        <span>NodeJS, Python Django, PostgreSQL, AWS</span>
+                    </li>
+                    <li className="item3">
+                        <h2>Languages</h2>
+                        <span>Java, Python, C, C++</span>
+                    </li>
+                    <li className="item4">
+                        <h2>Software</h2>
+                        <span>Git, Bitbucket, Jira, Trello, Slack</span>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Home
