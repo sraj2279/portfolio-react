da081edb971d119c0844638629a4320a0db483bb
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2022-11-19 13:45:55 +1100

    changed description of experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -15,7 +15,7 @@ function Experience() {
                     icon={<SchoolIcon />}>
                     <h3 className="vertical-timeline-element-title"> Bachelor of Computing | The University of Sydney</h3>
                     <h4 classname="vertical-timeline-element-subtitle">Sydney, New South Wales</h4>
-                    <p>Major in Computer Science, Minor in Software Development</p>
+                    <p>Major in Computer Science, Minor in Software Development.</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -24,7 +24,7 @@ function Experience() {
                     icon={<WorkIcon />}>
                     <h3 className="vertical-timeline-element-title"> Customer Service Team Member | BIG W</h3>
                     <h4 classname="vertical-timeline-element-subtitle">Sydney, New South Wales</h4>
-                    <p>Worked in the online section where items were collected and tagged to be packaged for online delivery</p>
+                    <p>Greeted customers with a friendly and helpful attitude. Answered any questions regarding inventory, sales, and promotions. Carefully and accurately rang up items, handled cash register and neatly packed customer bags. Worked well with other employees to ensure smooth processes.</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -33,7 +33,7 @@ function Experience() {
                     icon={<WorkIcon />}>
                     <h3 className="vertical-timeline-element-title"> Test Engineer | Wipro </h3>
                     <h4 classname="vertical-timeline-element-subtitle">Sydney, New South Wales</h4>
-                    <p>Performed various types of testing for wireless devices connected to the Telstra network</p>
+                    <p>Worked in a highly motivated team to enable introduction of new mobile devices (Smartphones, Tablets, Routers, IoT modules/devices) in market. Validated native apps, carrier specific apps, radio performance and protocol conformance aspects of LTE (4G) & New Radio (5G) technologies for the Telstra Network.</p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
